a4ec152f221aa6a3e12eeed60d962e74cd5cafac
Sahil Sahu <imsahilsahu11@gmail.com>
2024-07-02 00:47:51 +0530

    i hope so

--- app/(routes)/dashboard/budgets/_components/CreateBudget.tsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -8,101 +8,130 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
-import EmojiPicker from 'emoji-picker-react';
-import { Input } from '@/components/ui/input';
-import { Budgets } from '@/app/utils/schema';
-import { db } from '@/app/utils/db';
-import { useUser } from '@clerk/nextjs';
-import { toast } from '@/components/ui/use-toast';
-import { DialogClose } from '@radix-ui/react-dialog';
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import EmojiPicker from "emoji-picker-react";
+import { Input } from "@/components/ui/input";
+import { Budgets } from "@/app/utils/schema";
+import { db } from "@/app/utils/db";
+import { useUser } from "@clerk/nextjs";
+import { toast } from "@/components/ui/use-toast";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 type Props = {
-  refreshData:any,
-}
-
+  refreshData: any;
+};
+type Bud = {
+  id: number;
+  name: string;
+  amount: number;
+  createdBy: string;
+  icon: string;
+  // Other fields as needed
+};
 export default function CreateBudget({}: Props) {
-  
-  const [emojiIcon, setEmojiIcon] = useState('😊');
+  const [emojiIcon, setEmojiIcon] = useState<string>("😊");
   const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
 
-  const [name, setName] = useState<string>();
+  const [name, setName] = useState<string|any>("");
   const [amount, setAmount] = useState<number|any>();
-  const {user}=useUser();
+  const { user } = useUser();
 
+  // { name: string | SQL<unknown> | Placeholder<string, any>; 
+  // amount: string | SQL<unknown> | Placeholder<string, any>; 
+  // createdBy: string | SQL<unknown> | Placeholder<...>; id?: number | ... 2 more ... | undefined;
+  //  icon?: string | ... 3 more ... | undefined; }[]
 
-  
-  const onCreateBudget=async()=>{
-    const result=await db.insert(Budgets)
-    .values({
-      name:name,
-      amount:amount,
-      createdBy:user?.primaryEmailAddress?.emailAddress,
-      icon:emojiIcon,
-    }).returning({insertedId:Budgets.id})
-    if(result){
-      toast({
-        title:"Budget created!"
+  const onCreateBudget = async () => {
+    const userAddress:any=user?.primaryEmailAddress?.emailAddress
+
+    const result = await db
+      .insert(Budgets)
+      .values({
+        name: name,
+        amount: amount,
+        createdBy:userAddress ,
+        icon: emojiIcon,
       })
-    location.reload();
-
+      .returning({ insertedId: Budgets.id });
+    if (result) {
+      toast({
+        title: "Budget created!",
+      });
+      location.reload();
     }
-    
-  }
-  return(
+  };
+  return (
     <div>
       <Dialog>
-  <DialogTrigger className='w-full'>
-    <div className='border-2 bg-gray-400 border-dashed rounded-lg p-10 items-center 
-      flex flex-col cursor-pointer hover:shadow-2xl shadow-white'>
-        <h2 className='text-3xl'>+</h2>
-        <h2>Create Budget</h2>
-      </div>
-      </DialogTrigger>
-
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        <div className='mt-5'>
-          <Button variant="outline" className=' border-black text-2xl'
-          onClick={()=>setOpenEmojiPicker(!openEmojiPicker)}
-          >{emojiIcon}</Button>
-          
-        </div>
-        <div className='absolute z-20'>
-            <EmojiPicker  open={openEmojiPicker}
-            onEmojiClick={(e)=>{setEmojiIcon(e.emoji); setOpenEmojiPicker(false)}}
-            />
+        <DialogTrigger className="w-full">
+          <div
+            className="border-2 bg-gray-400 border-dashed rounded-lg p-10 items-center 
+      flex flex-col cursor-pointer hover:shadow-2xl shadow-white"
+          >
+            <h2 className="text-3xl">+</h2>
+            <h2>Create Budget</h2>
           </div>
-        <div className='mt-2'>
-          <h2 className='text-2xl text-black font-semibold my-1'>Create Budget</h2>
-          <Input placeholder='eg. Electricity bill' className='border-black'
-          onChange={(e)=>setName(e.target.value)}
-          />
-        </div>
-        <div className='mt-2'>
-          <h2 className='text-2xl text-black font-semibold my-1 '>Amount</h2>
-          <Input placeholder='eg. $500' type='number' className='border-black'
-          onChange={(e)=>setAmount(e.target.value)}
-          />
-        </div>
-        <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-           <Button disabled={!name || !amount} className='w-full mt-5' 
-        onClick={()=>onCreateBudget()}
-        >Create Budget</Button> 
-          </DialogClose>
-        </DialogFooter>
-        
+        </DialogTrigger>
 
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
-      
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              <div className="mt-5">
+                <Button
+                  variant="outline"
+                  className=" border-black text-2xl"
+                  onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+                >
+                  {emojiIcon}
+                </Button>
+              </div>
+              <div className="absolute z-20">
+                <EmojiPicker
+                  open={openEmojiPicker}
+                  onEmojiClick={(e) => {
+                    setEmojiIcon(e.emoji);
+                    setOpenEmojiPicker(false);
+                  }}
+                />
+              </div>
+              <div className="mt-2">
+                <h2 className="text-2xl text-black font-semibold my-1">
+                  Create Budget
+                </h2>
+                <Input
+                  placeholder="eg. Electricity bill"
+                  className="border-black"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="mt-2">
+                <h2 className="text-2xl text-black font-semibold my-1 ">
+                  Amount
+                </h2>
+                <Input
+                  placeholder="eg. $500"
+                  type="number"
+                  className="border-black"
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+              </div>
+              <DialogFooter className="sm:justify-start">
+                <DialogClose asChild>
+                  <Button
+                    disabled={!name || !amount}
+                    className="w-full mt-5"
+                    onClick={() => onCreateBudget()}
+                  >
+                    Create Budget
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
